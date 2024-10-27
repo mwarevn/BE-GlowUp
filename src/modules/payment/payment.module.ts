a6db452c2597c5ce05429ce341be9737dba845f9
Minhdd15112003 <minhdd15112003@gmail.com>
2024-10-27 23:07:39 +0700

    feat: :rocket: payment vnpay

--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -1,8 +1,34 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { VnpayModule } from 'nestjs-vnpay';
+import { ignoreLogger } from 'vnpay';
 
 @Module({
+  imports: [
+    VnpayModule,
+    VnpayModule.register({
+      tmnCode: 'REVSPUGM',
+      secureSecret: 'vZVDTGN3T8S2KBDUT7OGQCVAYLIDNL34Q',
+      vnpayHost: 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
+      testMode: true, // tùy chọn, ghi đè vnpayHost thành sandbox nếu là true
+
+      /**
+       * Sử dụng enableLog để bật/tắt logger
+       * Nếu enableLog là false, loggerFn sẽ không được sử dụng trong bất kỳ phương thức nào
+       */
+      enableLog: true, // tùy chọn
+
+      /**
+       * Hàm `loggerFn` sẽ được gọi để ghi log
+       * Mặc định, loggerFn sẽ ghi log ra console
+       * Bạn có thể ghi đè loggerFn để ghi log ra nơi khác
+       *
+       * `ignoreLogger` là một hàm không làm gì cả
+       */
+      loggerFn: ignoreLogger, // tùy chọn
+    }),
+  ],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
