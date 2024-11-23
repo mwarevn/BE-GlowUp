fe8b5504d4aa5f900f8e65c4746f75d1315e8f89
Minz <mwarevn@gmail.com>
2024-11-23 23:09:39 +0700

    Mer

--- src/main.ts
+++ src/main.ts
@@ -70,10 +70,10 @@ async function bootstrap() {
                 .catch(console.error);
         })
         .then(() => {
-            // console.clear();
+            console.clear();
             console.log('────────────────────────────────────────────────────────────────────────────────');
             console.log('\n');
-            // console.log(`[!] Make sure you have started the redis server (localhost and port: ${process.env.REDIS_PORT})!`);
+            console.log(`[!] Make sure you have started the redis server (localhost and port: ${process.env.REDIS_PORT})!`);
             console.log('\n');
             console.log(' > Application running on PORT: ' + PORT);
             console.log(' > Swagger running on: http://localhost:' + PORT + '/api-docs');
