d3195631c239b51859bdb47feb0f350e82274edc
Minz <mwarevn@gmail.com>
2024-11-23 23:56:59 +0700

    refactor: add alert for remind run redis server

--- src/main.ts
+++ src/main.ts
@@ -69,12 +69,12 @@ async function bootstrap() {
             console.clear();
             console.log('────────────────────────────────────────────────────────────────────────────────');
             console.log('\n');
-            // console.log(`[!] Make sure you have started the redis server (localhost and port: ${process.env.REDIS_PORT})!`);
+            console.log(`[!] Make sure you have started the redis server (localhost and port: ${process.env.REDIS_PORT})!`);
             console.log('\n');
             console.log(' > Application running on PORT: ' + PORT);
             console.log(' > Swagger running on: http://localhost:' + PORT + '/api-docs');
             console.log('\n\n');
-            console.log(new Date()); //
+            console.log(new Date());
         });
 }
 
