d7baca9f1a3d3d505247015ae532fd2f70092dc1
mwarevn <mwarevn@gmail.com>
2024-12-07 00:53:57 +0700

    setup CI/CD for production

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ git pull
 npm install
 cat example.env > .env
 npx prisma db push
-kill -9 $(lsof -t -i :3000) # kill old process
-npm run dev &
+kill $(lsof -t -i :3000) # kill old process
+nohup npm run dev &
